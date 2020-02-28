bb8d7f09021877e0ba702461dfb1159c5a7d8a30
Akbar <27790979+donatelloraphael@users.noreply.github.com>
2020-02-28 21:39:25 +0530

    finish top picks

--- components/Carousals/Cards/TopPicksCard.vue
+++ components/Carousals/Cards/TopPicksCard.vue
@@ -1,14 +1,36 @@
 <template>
 	<div class="top-container">
-		<div class="main" :style="{ 'background-image': 'url(' + mainUrl + ')' }">
-			
+		<div class="main" :style="{ 'background-image': 'url(' + mainUrl + ')' }" @click="logSelected()">
+			<div class="contents">
+				<p class="name">{{ topPicks[selected][0].nameDescription }}</p>
+				<p class="short-name">{{ topPicks[selected][0].company }}  {{ topPicks[selected][0].name }}</p>
+				<p class="specs">{{ topPicks[selected][0].specs }}</p>
+				<p class="price"><span class="label">Price: </span>Rs. {{ topPicks[selected][0].price }}</p>
+				<a  class="link-container":href="topPicks[selected][0].productUrl" target="_blank">
+					<div class="button">Shop now</div>
+				</a>
+			</div>
 		</div>
-		<div class="alt-1" :style="{ 'background-image': 'url(' + alt1Url + ')' }">
-			
-		</div>
-		<div class="alt-2" :style="{ 'background-image': 'url(' + alt2Url + ')' }">
-			
-		</div>
+	
+		<a  class="link-container":href="topPicks[selected][1].productUrl" target="_blank">
+			<div class="alt-1" :style="{ 'background-image': 'url(' + alt1Url + ')' }">
+				<div class="alt1-contents">
+					<p class="short-name">{{ topPicks[selected][1].company }}  {{ topPicks[selected][0].name }}</p>
+					<p class="specs">{{ topPicks[selected][1].specs }}</p>
+					<p class="price"><span class="label">Price: </span>Rs. {{ topPicks[selected][1].price }}</p>
+				</div>
+			</div>
+		</a>
+
+		<a  class="link-container":href="topPicks[selected][2].productUrl" target="_blank">
+			<div class="alt-2" :style="{ 'background-image': 'url(' + alt2Url + ')' }">
+				<div class="alt2-contents">
+					<p class="short-name">{{ topPicks[selected][2].company }}  {{ topPicks[selected][2].name }}</p>
+					<p class="specs">{{ topPicks[selected][2].specs }}</p>
+					<p class="price"><span class="label">Price: </span>Rs. {{ topPicks[selected][2].price }}</p>
+				</div>
+			</div>
+		</a>
 				
 	</div>
 	
@@ -27,7 +49,7 @@ export default {
 	},
 	methods: {
 		logSelected() {
-			console.log(this.selected);
+			console.log(this.topPicks);
 		}
 	},
 	watch: {
@@ -42,7 +64,7 @@ export default {
 </script>
 
 
-<style>
+<style scoped>
 	
 
 	.top-container {
@@ -51,6 +73,11 @@ export default {
 		height: 100%;
 		grid-template-areas: "main main alt-1"
 												 "main main alt-2";
+		font-family: "Montserrat", "Open Sans", sans-serif;
+	}
+
+	.link-container {
+		background-color: rgba(0,0,0,0);
 	}
 
 	.main {
@@ -58,13 +85,71 @@ export default {
 		grid-area: main;
 		min-width: 225px;
 		background: no-repeat center / 90%;
+		pointer: cursor;
+		height: 100%;
 	}
+
+	.contents {
+		width: 30%;
+		position: absolute;
+		margin: 20px 10px;
+		/*border-radius: 15px;*/
+		background-color: rgba(52, 176, 68, 0.8);
+		/*background-color: rgba(183, 183, 183, 0.5);*/
+		font-size: 1rem;
+		border-radius: 10px;
+	}
+
+	.name {
+		color: white;
+	}
+
+	.contents .short-name {
+		display: none;
+	}
+
+	p.specs {
+		font-size: 0.8rem;
+		color: white;
+	}
+
+	p.price {
+		color: yellow;
+	}
+
+	p {
+		margin: 5px 0;
+	}
+
+	.label {
+		font-size: 0.8rem;
+		color: white;
+	}
+
+	.button {
+		padding: 5px 10px;
+		margin: 0 0 5px 0;
+		background-color: blue;
+		cursor: pointer;
+		display: inline-block;
+		color: white;
+	}
+
 
 	.alt-1 {
 		/*background-color: red;*/
 		grid-area: alt-1;
 		min-width: 100px;
+		height: 90%;
 		background: no-repeat center / 100%;
+		pointer: cursor;
+	}
+
+	.alt1-contents {
+		color: white;
+		background-color: rgba(46, 49, 146, 0.8);
+		margin: 20px 10px;
+		border-radius: 10px;
 	}
 
 	.alt-2 {
@@ -72,8 +157,48 @@ export default {
 		grid-area: alt-2;
 		min-width: 100px;
 		background: no-repeat center / 100%;
+		pointer: cursor;
+		height: 90%;
 	}
-		
+
+	.alt2-contents {
+		color: white;
+		/*background-color: rgba(205, 11, 196, 0.5);*/
+		background-color: rgba(170, 0, 0, 0.8);
+		border-radius: 10px;
+		margin: 20px 10px;
+	}
+	
+	a:hover {
+		text-decoration: none;
+	}
+
+
+	/****************************MEDIA QUERYS*********************************/
+	@media (max-width: 1200px) {
+		.contents {
+			width: 40%;
+		}
+	}
+
+	@media (max-width: 1018px) {
+		.contents .name {
+			font-size: 0.9rem;
+		}
+	}
+
+	@media (max-width: 789px) {
+		.contents {
+			width: 55%;
+			font-size: 0.8rem;
+		}
+
+		.contents .name {
+			font-size: 0.8rem;
+		}
+
+	}
+
 	@media (max-width: 470px) {
 		.top-container {
 			display: flex;
@@ -84,7 +209,31 @@ export default {
 		.main {
 			width: 100%;
 			height: 60%;
-		}		
+		}
+
+		.contents {
+			width: 80%;
+			/*font-family: "Open Sans", sans-serif;*/
+			margin: 2% 5%;
+		}
+
+		.contents .short-name {
+			font-size: 1rem;
+			color: white;
+		}
+
+		p {
+			padding: 0;
+			margin: .2rem;
+		}
+
+		.contents .name {
+			display: none;
+		}
+
+		.contents .short-name {
+			display: block;
+		}
 
 		.alt-1 {
 			width: 50%;
