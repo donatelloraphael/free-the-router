cfd2c93fb9b36fc909be5ed3c07ec2599883aa59
Akbar <27790979+donatelloraphael@users.noreply.github.com>
2020-03-19 17:42:39 +0530

    implementing matching of advancedtomato routers to tomatobyshibby routers to copy details

--- functions/firmwareRouters/advancedtomato.js
+++ functions/firmwareRouters/advancedtomato.js
@@ -13,26 +13,62 @@ const db = admin.firestore();
 const pubSubClient = new PubSub();
 
 let routerList = [];
+tomatobyshibbyList = [];
 const tomatoRef = db.collection("tomatobyshibby-main-list");
+const advancedtomatoRef = db.collection("advancedtomato-main-list");
 
 exports.createAdvancedtomatoList = function() {
 
 }
 
-axios.get("https://advancedtomato.com/downloads")
-	.then((res) => {
-		$(".router-name", res.data).each((i, element) => {
-			routerList.push({ fullName: $(element).text() });
+async function createAdvancedtomato() {
+	await axios.get("https://advancedtomato.com/downloads")
+		.then((res) => {
+			$(".router-name", res.data).each((i, element) => {
+				routerList.push($(element).text());
+			})
+		}).then(() => {
+			return true;
 		})
-	}).then(() => {
-		console.log(routerList);
-	})
+
+	tomatobyshibbyList = await tomatoRef.get()
+												.then((querySnapshot) => {
+													querySnapshot.forEach((doc) => {
+														tomatobyshibbyList.push(doc.data());
+													});
+													return tomatobyshibbyList;
+												})
+	
+	let nameIndex = await advancedtomatoRef.doc("index").get()
+								.then((doc) => {
+									return doc.data().fullNameIndex;
+								})
+
+
+	routerListLength = routerList.length;
+	shibbyLength = tomatobyshibbyList.length;
+
+	for (let i = 0; i < routerListLength; i++) {
+		if (!nameIndex.includes(routerList[i])) {
+			routerListParsed = routerList[i].split(/[\s,.&\(]+/gi).join("");
+			console.log("+: ", routerListParsed);
+			for (let j = 0; j < shibbyLength - 1; j++) {
+				shibbyParsed = tomatobyshibbyList[j].fullName.split(/[\s,.&\(]+/gi).join("");
+				console.log("-: ", shibbyParsed);
+
+			}
+		}
+	}
+}
+
 
 	
 
 tomatoRef.get()
 	.then((querySnapshot) => {
 		querySnapshot.forEach((doc) => {
-			// console.log(doc.data());
+			
 		})
 	});
+
+createAdvancedtomato();
