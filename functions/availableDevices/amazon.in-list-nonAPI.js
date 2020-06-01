532183ce79c999a32cdceb0c14f65998432ee3cc
Akbar <27790979+donatelloraphael@users.noreply.github.com>
2020-06-02 05:28:33 +0530

    detect end of amazon pages

--- functions/availableDevices/amazon.in-list-nonAPI.js
+++ functions/availableDevices/amazon.in-list-nonAPI.js
@@ -15,6 +15,7 @@ const allFirmwareRoutersRef = db.collection("all-firmware-routers");
 
 async function getDevices() {
 
+	const amazonRouters = "https://www.amazon.in/s?i=computers&rh=n%3A976392031%2Cn%3A976393031%2Cn%3A1375427031%2Cn%3A1375439031&qid=1590958154&page=";
 	let fullNameIndex = [];
 
 	await indicesRef.doc("all-routers-index").get()
@@ -24,7 +25,46 @@ async function getDevices() {
 		}
 	});
 
-	console.dir(fullNameIndex, {maxArrayLength: null});
+	// console.dir(fullNameIndex, {maxArrayLength: null});
+
+
+	let deviceArray = [];
+	let numberOfDevices = 0;
+
+
+	// for (let page = 1;;page++) {
+
+		let isPageContains = false;
+
+		// console.log("PAGE: ", page);
+
+		await axios.get(amazonRouters + 30)
+		.then(res => {
+
+			// $(".s-result-item", res.data).each((i, element) => {
+			// 	console.log($(element).html());
+			// });
+				
+			if ($("span", ".s-result-item", res.data).attr("class") == "celwidget slot=MAIN template=TOP_BANNER_MESSAGE widgetId=messaging-messages-no-results") {
+				console.log("No more items.");
+			} else {
+				console.log('has items');
+			}
+
+			console.log("YES");
+
+			// 	// console.log($(element).html());
+			// 	numberOfDevices++;
+			// 	console.log("SERIAL NO: ", numberOfDevices);
+			// 	isPageContains = true;
+			// });
+		});
+
+		// if (!isPageContains) {
+		// 	console.log('__________End of Pages.____________');
+		// 	break;
+		// }
+	// }
 
 }
 
