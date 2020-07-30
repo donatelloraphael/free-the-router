e8c834c53c2abd3dcdededf6d6b52cb35c9c6e4b
Akbar <27790979+donatelloraphael@users.noreply.github.com>
2020-07-31 03:54:38 +0530

    fixed firbase.json

--- src/middleware/setCountry.js
+++ src/middleware/setCountry.js
@@ -9,19 +9,16 @@ export default function ({ req, res, store, redirect, params, route }) {
 			store.dispatch("setCountry", country);
 			const countryRegex = new RegExp(`(\/${params.country}\/?|$)`);
 			return redirect(route.fullPath.replace(countryRegex, "/" + country + "/"));
-
-		} else {
 			
-			if (route.path != "/sitemap.xml") {
-				let ip = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',').shift() : req.connection.remoteAddress;
+		} else {
+			let ip = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',').shift() : req.connection.remoteAddress;
 
-				return axios.get("https://geolocation-db.com/json/" + ip)
-				.then(result => {
-					store.dispatch("setCountry", result.data.country_code);	
-				}).then(() => {
-					return redirect(`/${store.getters.getCountry}/`);
-				}).catch(error => console.log(error));
-			}
+			return axios.get("https://geolocation-db.com/json/" + ip)
+			.then(result => {
+				store.dispatch("setCountry", result.data.country_code);	
+			}).then(() => {
+				return redirect(`/${store.getters.getCountry}/`);
+			}).catch(error => console.log(error));
 						
 		}
 	}
