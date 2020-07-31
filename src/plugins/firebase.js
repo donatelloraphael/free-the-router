1f4d77c3d32ded818406eafc1e4072cea78b439c
Akbar <27790979+donatelloraphael@users.noreply.github.com>
2020-07-31 20:38:52 +0530

    perf

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,14 +2,15 @@ import * as firebase from "firebase/app";
 
 import "firebase/firestore";
 import "firebase/analytics";
+import "firebase/performance";
 
 import {FIREBASE_CONFIG} from "../../env";
 
 if (!firebase.apps.length) {   
-
     firebase.initializeApp(FIREBASE_CONFIG);
-    firebase.firestore();
 }
+
+const perf = firebase.performance();
 
 const db = firebase.firestore();
 
