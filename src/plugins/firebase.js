43aba4b0c0a60a8410731ebbf321963a2a5fc673
Akbar <27790979+donatelloraphael@users.noreply.github.com>
2020-07-31 20:54:01 +0530

    moved analytics initiation

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,16 +2,12 @@ import * as firebase from "firebase/app";
 
 import "firebase/firestore";
 import "firebase/analytics";
-import "firebase/performance";
 
 import {FIREBASE_CONFIG} from "../../env";
 
 if (!firebase.apps.length) {   
     firebase.initializeApp(FIREBASE_CONFIG);
 }
-
-const perf = firebase.performance();
-
 const db = firebase.firestore();
 
 export { db, firebase };
