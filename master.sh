530d4a5d4950288e3d9a87548c85baaab1487f30
Akbar <27790979+donatelloraphael@users.noreply.github.com>
2020-09-27 17:50:23 +0530

    migration chores

--- master.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git checkout master
-cp ./firebase-adminsdk-prod.json ./functions/firebase-adminsdk.json
-cp ./firebase-adminsdk-prod.json ./functions-local/firebase-adminsdk.json
-cp env-prod.js env.js
-cp firebaserc-prod .firebaserc
-firebase use free-the-router-production
