e83f36d767c26201843b600cf15895267875c4d4
Akbar <27790979+donatelloraphael@users.noreply.github.com>
2020-09-29 20:38:56 +0530

    fix mongoSync

--- firebase/functions/mongo-sync.js
+++ firebase/functions/mongo-sync.js
@@ -16,8 +16,8 @@ const db = admin.firestore();
 
 const uri = `mongodb+srv://defaultReadWrite:${MONGO_PWD}@freetherouter.dm5jh.mongodb.net/freetherouter?retryWrites=true&w=majority`;
 
-// exports.mongoSync = async function() {
-async function mongoSync() {
+exports.mongoSync = async function() {
+// async function mongoSync() {
 
 	/**********************************************************************/
 	// Get devices from firestore																					//
@@ -41,23 +41,28 @@ async function mongoSync() {
 	/**********************************************************************/
 	// Upload devices to mongoDB																					//
 	/**********************************************************************/
+	try {
+		const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+		await client.connect();
 
-	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-	await client.connect();
+		const mdb = client.db("freetherouter");
+		const bulkOp = mdb.collection("all-firmware-devices").initializeUnorderedBulkOp();
 
-	const mdb = client.db("freetherouter");
-	const bulkOp = mdb.collection("all-firmware-devices").initializeUnorderedBulkOp();
+		let arrLength = allRouters.length;
+		for (let i = 0; i < arrLength; i++) {
+			bulkOp.find({fullName: allRouters[i].fullName}).upsert().updateOne({ $set: allRouters[i] });
+		}
 
-	let arrLength = allRouters.length;
-	for (let i = 0; i < arrLength; i++) {
-		bulkOp.find({fullName: allRouters[i].fullName}).upsert().updateOne({ $set: allRouters[i] });
+		await bulkOp.execute();
+
+		await mdb.collection("indices").updateOne({ name: "all-devices-index"}, { $set: {fullNameIndex: allRoutersIndex} }, {upsert: true});
+
+		console.log('Sync to MongoDB Success.');
+		client.close();
+		
+	} catch (error) {
+		console.log(error);
 	}
-
-	await bulkOp.execute();
-
-	await mdb.collection("indices").updateOne({ name: "all-devices-index"}, { $set: {fullNameIndex: allRoutersIndex} }, {upsert: true});
-
-	client.close();
 };
 
-mongoSync();
+// mongoSync();
---
+++ server/nuxt/env.js
@@ -0,0 +1,10 @@
+export const FIREBASE_CONFIG = {
+			apiKey: "AIzaSyCZw8VsxETOXacf8dRHtx87MPz7jc_1-N8",
+		  authDomain: "free-the-router-production.firebaseapp.com",
+		  databaseURL: "https://free-the-router-production.firebaseio.com",
+		  projectId: "free-the-router-production",
+		  storageBucket: "free-the-router-production.appspot.com",
+		  messagingSenderId: "15965106844",
+		  appId: "1:15965106844:web:5175814c7dec1c0c447359",
+		  measurementId: "G-BFWRJQ552V"
+};
