ed41fdab0a45373559d64d89938b0e9979ec8259
Akbar <27790979+donatelloraphael@users.noreply.github.com>
2020-10-02 02:07:58 +0530

    add redis-mongo sync

--- server/background/env.js
+++ server/background/env.js
@@ -1,1 +1,3 @@
 exports.MONGO_PWD = "dVG6tn5hhhXxtwN";
+exports.MONGO_PWD_READ =  "ZrE8WA3CsxvRa56P";
+exports.REDIS_PWD = "werqgtln3asdOJwAS";
---
+++ server/background/redis-sync.js
@@ -0,0 +1,37 @@
+// Sync collections from MongoDB to Redis
+
+const MONGO_PWD_READ = require("./env").MONGO_PWD_READ;
+const REDIS_PWD = require("./env").REDIS_PWD;
+const MongoClient = require('mongodb').MongoClient;
+
+const Redis = require("ioredis");
+const redis = new Redis({ password: REDIS_PWD });
+
+const uri = `mongodb+srv://readOnly:${MONGO_PWD_READ}@freetherouter.dm5jh.mongodb.net/freetherouter?retryWrites=true&w=majority`;
+
+async function redisSync() {
+
+		const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+		await client.connect();
+		const mdb = client.db("freetherouter");
+
+		const collections = await mdb.listCollections({}, {nameOnly: true}).toArray();
+
+		for (let i = 0, length = collections.length; i < length; i++) {
+
+			await mdb.collection(collections[i].name).find().toArray()
+			.then(async docs => {
+				await redis.del(collections[i].name);
+				docs.forEach(async device => {
+					await redis.rpush(collections[i].name, JSON.stringify(device));
+				});
+				console.log(`${collections[i].name} - collection synced`);
+			}).catch(error => console.log(error));
+		}
+
+
+		client.close();
+		redis.disconnect();
+}
+
+redisSync();
